c931017896e14d7b4b614f0836b7eb0afe7cb896
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-05-22 15:55:25 +0200

    css ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,17 @@ ingredients['drinks'].each do |ingredient|
   Ingredient.create(name: ingredient['strIngredient1'])
 end
 
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Whisky sour")
-Cocktail.create(name: "Spritz")
+url = 'https://cdn.liquor.com/wp-content/uploads/2018/09/04153106/mojito-720x720-recipe.jpg'
+mojito = Cocktail.new(name: 'Mojito')
+mojito.remote_photo_url = url
+mojito.save
+
+url = 'https://static.oldforester.com/app/uploads/2017/05/01201328/WhiskeySourEgg-Env.jpg'
+whisky = Cocktail.new(name: 'Whisky Sour')
+whisky.remote_photo_url = url
+whisky.save
+
+url = 'https://www.chatelaine.com/wp-content/uploads/2018/06/Classic-aperol-spritz-800x800.jpg'
+spritz = Cocktail.new(name: 'Spritz')
+spritz.remote_photo_url = url
+spritz.save
